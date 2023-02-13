88c6cb491e5a37c3385e3ec98e2856b01bedeca0
Seth Battis <sbattis@groton.org>
2023-02-13 21:28:31 +0000

    npm instead of pnpm
    https://github.com/pnpm/pnpm/issues/801

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
 const path = require('path');
-const GasPlugin = require('gas-webpack-plugin');
+const config = require('@battis/webpack-typescript-gas');
 
-module.exports = {
-  mode: 'production',
-  entry: {
-    main: './src/index.ts',
-  },
-  output: {
-    path: path.resolve(__dirname, './build'),
-    filename: '[name]-bundle.js',
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: { allowTsInNodeModules: true },
-      },
-    ],
-  },
-  plugins: [new GasPlugin()],
-};
+config.output.path = path.join(__dirname, 'build');
+
+module.exports = config;
