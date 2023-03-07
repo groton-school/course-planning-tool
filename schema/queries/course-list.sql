b1e6c78d2521cfe365261e9641c30c5c5a9e83a4
Seth Battis <sbattis@groton.org>
2023-03-07 21:03:08 +0000

    schema data

---
+++ schema/queries/course-list.sql
@@ -0,0 +1,3 @@
+select dcb1.course_id as [Course ID] , dcb1.course_code as [Course Code] , dcb1.course_title as [Course Title (with term)] , aca1.department as [Department] , dcd1.description as [Description] from
+dbo.da_course_base(@p1)dcb1 INNER JOIN dbo.da_course_department(@p1)cd1 ON cd1.course_id = dcb1.course_id INNER JOIN dbo.da_academic_department(@p1)aca1 ON aca1.department_id = cd1.department_id INNER JOIN dbo.da_course_detail(@p1)dcd1 ON dcd1.course_id = dcb1.course_id
+where dcb1.inactive = 0 And dcd1.independent_study = 0
